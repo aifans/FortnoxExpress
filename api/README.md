7feff485256be8980e8c3079a6055711673cb90b
Evan <evan@aifans.net>
2021-09-22 07:32:28 +0200

    add: explain of @WebLog .

--- api/README.md
+++ api/README.md
@@ -1,1 +1,19 @@
-# FortnoxExpress
+# FortnoxExpress API Server
+
+The project uses Maven as build tool. You can also use the IDE such like IntelliJ or Eclipse to run it easier..
+
+*DB*
+
+- Run the db.sql file on your MySQL
+
+*API project*
+
+- Open the project in IntelliJ
+- Navigate and change src/main/resources/application-dev.properties
+    - Replace the db settings as necessary
+- The API should now be running on localhost:8070
+
+*@WebLog*
+
+- The API controller methods decorated by this annotation will print the detailed information of the call. It only takes effect in dev and test environments.
+- You can set resources/application.properties to dev, test, prod.
--- api/src/main/java/se/fortnox/codetest/fortnoxexpress/controller/CustomErrorController.java
+++ api/src/main/java/se/fortnox/codetest/fortnoxexpress/controller/CustomErrorController.java
@@ -4,12 +4,15 @@ package se.fortnox.codetest.fortnoxexpress.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import se.fortnox.codetest.fortnoxexpress.aspect.WebLog;
 import se.fortnox.codetest.fortnoxexpress.exception.BizException;
 import se.fortnox.codetest.fortnoxexpress.exception.ErrorEnum;
 
 @Controller
 @RequestMapping("${server.error.path:${error.path:/error}}")
 public class CustomErrorController implements ErrorController {
+
+    @WebLog(description = "custom error controller")
     @RequestMapping
     public void error() throws Exception {
         throw new BizException(ErrorEnum.NOT_FOUND);
